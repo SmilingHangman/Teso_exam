9d3f3b94c93a22e6343598ed96c2801635a0b852
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-09-26 12:19:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
+  const board = [...Array(10)].map(() => [...Array(10)].map(() => 0))
+
+  const board2 = [
+    ...Array(10)
+      .fill(<div className='board-row'></div>)
+      .map(() => new Array(10).fill(<div className='board-cell'></div>)),
+  ]
+
+  console.log(board)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <button>Turn Right</button>
+        <button>Move Forward</button>
+      </div>
+      <div className='board-box'>{board}</div>
+      <div>{board2}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
